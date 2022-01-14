d26f9c9be4dc9fad4a657f6cabc9c21df011d7fc
unknown <junalsina6@gmail.com>
2022-01-13 19:41:11 -0800

    included android apk download link

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -1,30 +1,30 @@
-import * as React from 'react';
-import CssBaseline from '@mui/material/CssBaseline';
-import Stack from '@mui/material/Stack';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import Link from '@mui/material/Link';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import Header from './Header';
-import CustomizedButtons from './Button';
+import * as React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Link from "@mui/material/Link";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Header from "./Header";
+import CustomizedButtons from "./Button";
 
 function Copyright() {
   return (
     <Typography variant="body2" color="text.secondary" align="center">
-      {'Copyright © '}
+      {"Copyright © "}
       <Link color="inherit" href="https://mui.com/">
         veet
-      </Link>{' '}
+      </Link>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
   );
 }
 
 const theme = createTheme();
 
-export default function Main({title}) {
+export default function Main({ title }) {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
@@ -33,7 +33,7 @@ export default function Main({title}) {
         {/* Hero unit */}
         <Box
           sx={{
-            bgcolor: 'background.paper',
+            bgcolor: "background.paper",
             pt: 8,
             pb: 6,
           }}
@@ -48,41 +48,65 @@ export default function Main({title}) {
             >
               Easily identify foods around
             </Typography>
-            <Typography variant="h6" align="center" color="text.secondary" paragraph>
-              Get all variety of foods you are looking for from various 
-              eatries around you. View all eatries around, view their
-              various menu, prices and easily contact them
-              to place your order.
-            </Typography>
-            <div style={{marginTop: 50, border: "1px solid #61C890"}}>
-            <h3 style={{textAlign: "center"}}>Veet Beta</h3>
-            <Typography variant="h6" align="center" color="text.secondary" paragraph>
-              Would you like to join the beta production? then download app from
-              the section below.
-            </Typography>
-            </div>
-            <h3 style={{textAlign: "center", marginTop: 60}}>Download App</h3>
-            <div className='stack-btn'>
-            <Stack
-              sx={{ pt: 4 }}
-              direction="row"
-              spacing={2}
-              justifyContent="center"
+            <Typography
+              variant="h6"
+              align="center"
+              color="text.secondary"
+              paragraph
             >
-            <CustomizedButtons title="PlayStore" />  
-            <CustomizedButtons title="AppStore" />
-            </Stack>
-            <Typography style={{marginTop: 20}} variant="h6" align="center" color="text.secondary" paragraph>
-              Thank you for trying it out.
+              Get all variety of foods you are looking for, from various eatries
+              around you. View all eatries around, view their various menu,
+              prices and easily contact them to place your order.
             </Typography>
+            <div style={{ marginTop: 50, border: "1px solid #61C890" }}>
+              <h3 style={{ textAlign: "center" }}>Veet Beta</h3>
+              <Typography
+                variant="h6"
+                align="center"
+                color="text.secondary"
+                paragraph
+              >
+                Would you like to join the beta production? then download app
+                from the section below.
+              </Typography>
+            </div>
+            <h3 style={{ textAlign: "center", marginTop: 60 }}>Download App</h3>
+            <div className="stack-btn">
+              <Stack
+                sx={{ pt: 4 }}
+                direction="row"
+                spacing={2}
+                justifyContent="center"
+              >
+                <a
+                  style={{ textDecoration: "none" }}
+                  href="https://firebasestorage.googleapis.com/v0/b/veet-de560.appspot.com/o/veet.apk?alt=media&token=8c927505-d5c8-4c4b-887f-55f5e51d3703"
+                >
+                  <CustomizedButtons title="Android" />
+                </a>
+                <CustomizedButtons title="iPhone" />
+              </Stack>
+              <Typography
+                style={{ marginTop: 20 }}
+                variant="h6"
+                align="center"
+                color="text.secondary"
+                paragraph
+              >
+                Thank you for trying it out.
+              </Typography>
             </div>
           </Container>
         </Box>
       </main>
       {/* Footer */}
-      <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
+      <Box sx={{ bgcolor: "background.paper", p: 6 }} component="footer">
         <Typography variant="h6" align="center" gutterBottom>
-        <img style={{ width: 50, height: "auto" }} alt="logo" src="icon.png" />
+          <img
+            style={{ width: 50, height: "auto" }}
+            alt="logo"
+            src="icon.png"
+          />
         </Typography>
         <Typography
           variant="subtitle1"
