f421047a6d15fd831fa82596083639df79ec7693
junalsina <97355770+junalsina@users.noreply.github.com>
2022-03-20 12:19:47 +0000

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,15 @@
 import Main from './components/Main';
 import "./components/styles.css";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import firebaseConfig from './firebase';
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 function App() {
   return (
     <div className="font">
