1afb66f91b30136f95e52fcd18cf192d597c0df2
junalsina <97355770+junalsina@users.noreply.github.com>
2022-03-20 12:18:31 +0000

    Create firebase.js

---
+++ frontend/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyByuxyFOK63cJFtaZNyavnHHnYn5sEsyMU",
+    authDomain: "veetgh-web.firebaseapp.com",
+    projectId: "veetgh-web",
+    storageBucket: "veetgh-web.appspot.com",
+    messagingSenderId: "960432421858",
+    appId: "1:960432421858:web:bfa7569b316b82f9b897bb",
+    measurementId: "G-1H1FL0KQYN"
+  };
+
+  export default firebaseConfig;
